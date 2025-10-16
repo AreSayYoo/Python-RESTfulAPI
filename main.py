5c775e59cb0fc847da4c5fd8e022cdc1ac53358e
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2025-10-15 20:08:25 -0400

    feat(api)!: added new fields and implemented Pydantic validations for all fields in Person Class

--- main.py
+++ main.py
@@ -7,24 +7,45 @@ warnings.filterwarnings(
 )
 
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, AfterValidator, BeforeValidator
+from typing import Annotated, Optional, Set
 
 app = FastAPI()
 
+def validate_email(email: str, info):
+    email = email.strip().lower()
+    if "@" not in email:
+        raise HTTPException(status_code=422, detail="Invalid email address")
+    return email
+
+def validate_name(name: str, info):
+    # strip spaces
+    name = name.strip()
+    if not name.isalpha():
+        raise HTTPException(status_code=422, detail="Name must contain only alphabetic characters")
+    return name
+
+ValidatedName = Annotated[str, BeforeValidator(validate_name)]
+
 class Person(BaseModel):
     id: int
-    firstname: str
-    lastname: str 
+    firstname: ValidatedName
+    lastname: ValidatedName
+    email: Annotated[str, BeforeValidator(validate_email)] = None
+    favorite_anime: Optional[str] = None
+    model_config = {"validate_assignment": True}
 
 _people = [
     Person(id=0, firstname="John", lastname="Doe"),
     Person(id=1, firstname="Matt", lastname="A"),
     Person(id=2, firstname="Joe", lastname="Blow")
 ]
+_free_ids: Set[int] = set()
 
 def get_next_id() -> int:
-    if _people:
+    if _free_ids:
+        return min(_free_ids)
+    elif _people:
         return max(person.id for person in _people) + 1
     return 0
 
@@ -44,19 +65,23 @@ def get_person(person_id: int):
     raise HTTPException(status_code=404, detail="Person not found")
 
 @app.post("/v1/api/people")
-def create_person(firstname: str, lastname: str):
-    new_person = Person(id = get_next_id(), firstname=firstname, lastname=lastname)
+def create_person(firstname: str, lastname: str, email: str = None, favorite_anime: str = None):
+    new_person = Person(id = get_next_id(), firstname=firstname, lastname=lastname, email=email, favorite_anime=favorite_anime)
     _people.append(new_person)
     return new_person
 
 @app.put("/v1/api/people/{person_id}")
-def update_person(person_id: int, firstname: str = None, lastname: str = None):
+def update_person(person_id: int, firstname: str = None, lastname: str = None, email: str = None, favorite_anime: str = None):
     for person in _people:
         if person.id == person_id:
             if firstname:
                 person.firstname = firstname
             if lastname:
                 person.lastname = lastname
+            if email:
+                person.email = email
+            if favorite_anime:
+                person.favorite_anime = favorite_anime
             return person
     raise HTTPException(status_code=404, detail="Person not found")
 
@@ -65,5 +90,6 @@ def delete_person(person_id: int):
     for person in _people:
         if person.id == person_id:
             _people.remove(person)
+            _free_ids.add(person.id)
             return person
     raise HTTPException(status_code=404, detail="Person not found")
