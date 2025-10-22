e49febf35a6f4b6501df702c5ba7cc2816165808
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2025-10-21 21:28:48 -0400

    build: fixed pip commmands in ruff.yml

--- main.py
+++ main.py
@@ -12,29 +12,36 @@ warnings.filterwarnings(
 
 app = FastAPI()
 
+
 def validate_name(name: str, info):
     # strip spaces
     name = name.strip()
     if not name.isalpha():
-        raise HTTPException(status_code=422, detail="Name must contain only alphabetic characters")
+        raise HTTPException(
+            status_code=422, detail="Name must contain only alphabetic characters"
+        )
     return name
 
+
 ValidatedName = Annotated[str, BeforeValidator(validate_name)]
+
 
 class Person(BaseModel):
     id: int
     firstname: ValidatedName
     lastname: ValidatedName
-    email: EmailStr | None 
+    email: EmailStr | None
     favorite_anime: str | None = None
     model_config = {"validate_assignment": True}
+
 
 _people = [
     Person(id=0, firstname="John", lastname="Doe", email="matt@aresayoo.com"),
     Person(id=1, firstname="Matt", lastname="A", email=None),
-    Person(id=2, firstname="Joe", lastname="Blow", email=None)
+    Person(id=2, firstname="Joe", lastname="Blow", email=None),
 ]
 _free_ids: set[int] = set()
+
 
 def get_next_id() -> int:
     if _free_ids:
@@ -43,15 +50,17 @@ def get_next_id() -> int:
         return max(person.id for person in _people) + 1
     return 0
 
+
 @app.get("/")
 def read_root():
     return {"message": "RESTfulAPI is running!"}
 
-@app.get("/api/v1/people",response_model=list[Person])
+
+@app.get("/api/v1/people", response_model=list[Person])
 def get_people(
     favorite_anime: str | None = None,
-    hasEmail: bool | None = Query(default=None), 
-    hasFavoriteAnime: bool | None = Query(default=None)
+    hasEmail: bool | None = Query(default=None),
+    hasFavoriteAnime: bool | None = Query(default=None),
 ):
     people = _people
 
@@ -62,8 +71,13 @@ def get_people(
     for field, value in filters.items():
         if value:
             # if we are filtering and remove case-sensitive check
-            people = [p for p in people if getattr(p, field, None) and value.lower() == getattr(p, field).lower()]
-    
+            people = [
+                p
+                for p in people
+                if getattr(p, field, None)
+                and value.lower() == getattr(p, field).lower()
+            ]
+
     if hasEmail is True:
         people = [p for p in people if p.email]
     elif hasEmail is False:
@@ -72,24 +86,41 @@ def get_people(
         people = [p for p in people if p.favorite_anime]
     elif hasFavoriteAnime is False:
         people = [p for p in people if not p.favorite_anime]
-        
+
     return people
 
-@app.get("/api/v1/people/{person_id}",response_model=Person)
+
+@app.get("/api/v1/people/{person_id}", response_model=Person)
 def get_person(person_id: int):
     for person in _people:
         if person.id == person_id:
             return person
     raise HTTPException(status_code=404, detail="Person not found")
 
-@app.post("/api/v1/people",response_model=Person,status_code=201)
-def create_person(firstname: str, lastname: str, email: str = None, favorite_anime: str = None):
-    new_person = Person(id = get_next_id(), firstname=firstname, lastname=lastname, email=email, favorite_anime=favorite_anime)
+
+@app.post("/api/v1/people", response_model=Person, status_code=201)
+def create_person(
+    firstname: str, lastname: str, email: str = None, favorite_anime: str = None
+):
+    new_person = Person(
+        id=get_next_id(),
+        firstname=firstname,
+        lastname=lastname,
+        email=email,
+        favorite_anime=favorite_anime,
+    )
     _people.append(new_person)
     return new_person
 
-@app.put("/api/v1/people/{person_id}",response_model=Person)
-def update_person(person_id: int, firstname: str = None, lastname: str = None, email: str = None, favorite_anime: str = None):
+
+@app.put("/api/v1/people/{person_id}", response_model=Person)
+def update_person(
+    person_id: int,
+    firstname: str = None,
+    lastname: str = None,
+    email: str = None,
+    favorite_anime: str = None,
+):
     for person in _people:
         if person.id == person_id:
             if firstname:
